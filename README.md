4701609ce77f7a2d0a4335f302a56b91d997bc62
keon <kwk236@gmail.com>
2017-12-03 15:36:31 -0500

    remove visdom

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # seq2seq
 
-spacy
+Minimal seq2seq implementation
 
+
+
+
+## Requirements
+
+* Python3
+* PyTorch
+* torchtext
+* Spacy
+* numpy
+* Visdom (optional)
+
+download tokenizers by doing so:
 ```
 sudo python3 -m spacy download de
 sudo python3 -m spacy download en
--- train.py
+++ train.py
@@ -8,7 +8,6 @@ from torch.nn.utils import clip_grad_norm
 from torch.nn import functional as F
 from model import Encoder, Decoder, Seq2Seq
 from utils import load_dataset
-from logger import VisdomWriter, log_samples
 
 
 def parse_arguments():
@@ -40,7 +39,7 @@ def evaluate(model, val_iter, vocab_size, DE, EN):
     return total_loss / len(val_iter)
 
 
-def train(e, model, optimizer, train_iter, vocab_size, grad_clip, vis, DE, EN):
+def train(e, model, optimizer, train_iter, vocab_size, grad_clip, DE, EN):
     model.train()
     total_loss = 0
     pad = EN.vocab.stoi['<pad>']
@@ -61,12 +60,7 @@ def train(e, model, optimizer, train_iter, vocab_size, grad_clip, vis, DE, EN):
             total_loss = total_loss / 500
             print("[%d][loss:%5.2f][pp:%5.2f]" %
                   (b, total_loss, math.exp(total_loss)))
-            vis.update(total_loss)
             total_loss = 0
-    if not os.path.isdir(".samples"):
-        os.makedirs(".samples")
-    log_samples('./.samples/de-%d.txt' % e, src.data, DE, is_sample=False)
-    log_samples('./.samples/en-%d.txt' % e, output, EN)
 
 
 def main():
@@ -74,10 +68,6 @@ def main():
     hidden_size = 512
     embed_size = 256
     assert torch.cuda.is_available()
-
-    # visdom for plotting
-    vis_train = VisdomWriter("Training Loss", xlabel='Batch', ylabel='Loss')
-    vis_val = VisdomWriter("Validation Loss", xlabel='Epoch', ylabel='Loss')
 
     print("[!] preparing dataset...")
     train_iter, val_iter, test_iter, DE, EN = load_dataset(args.batch_size)
@@ -96,9 +86,8 @@ def main():
     best_val_loss = None
     for e in range(1, args.epochs+1):
         train(e, seq2seq, optimizer, train_iter,
-              en_size, args.grad_clip, vis_train, DE, EN)
+              en_size, args.grad_clip, DE, EN)
         val_loss = evaluate(seq2seq, val_iter, en_size, DE, EN)
-        vis_val.update(val_loss)
         print("[Epoch:%d] val_loss:%5.3f | val_pp:%5.2fS"
               % (e, val_loss, math.exp(val_loss)))
 
@@ -110,6 +99,8 @@ def main():
             torch.save(seq2seq.state_dict(), './.save/seq2seq_%d.pt' % (e))
             best_val_loss = val_loss
     test_loss = evaluate(seq2seq, test_iter, en_size, DE, EN)
+    print("[TEST] loss:%5.2f" % test_loss)
+
 
 if __name__ == "__main__":
     try:
