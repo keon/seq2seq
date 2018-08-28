44e32fe075438dadfb4042e4897daa3dcadcb891
Prabhsimran Singh <pskrunner14@gmail.com>
2018-08-29 04:17:20 +0530

    adds non-linearity to attn weights

--- model.py
+++ model.py
@@ -44,7 +44,7 @@ class Attention(nn.Module):
 
     def score(self, hidden, encoder_outputs):
         # [B*T*2H]->[B*T*H]
-        energy = self.attn(torch.cat([hidden, encoder_outputs], 2))
+        energy = F.softmax(self.attn(torch.cat([hidden, encoder_outputs], 2)))
         energy = energy.transpose(1, 2)  # [B*H*T]
         v = self.v.repeat(encoder_outputs.size(0), 1).unsqueeze(1)  # [B*1*H]
         energy = torch.bmm(v, energy)  # [B*1*T]
