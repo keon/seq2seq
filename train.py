2321f8789713a086978f6e37d088bd00aa60e5f0
Amit Moryossef <amitmoryossef@gmail.com>
2020-03-07 17:54:47 +0200

    fix(evaluate): use no_grad over volatile=True
    volatile=True is deprecated

--- train.py
+++ train.py
@@ -24,20 +24,21 @@ def parse_arguments():
 
 
 def evaluate(model, val_iter, vocab_size, DE, EN):
-    model.eval()
-    pad = EN.vocab.stoi['<pad>']
-    total_loss = 0
-    for b, batch in enumerate(val_iter):
-        src, len_src = batch.src
-        trg, len_trg = batch.trg
-        src = Variable(src.data.cuda(), volatile=True)
-        trg = Variable(trg.data.cuda(), volatile=True)
-        output = model(src, trg, teacher_forcing_ratio=0.0)
-        loss = F.nll_loss(output[1:].view(-1, vocab_size),
-                               trg[1:].contiguous().view(-1),
-                               ignore_index=pad)
-        total_loss += loss.data[0]
-    return total_loss / len(val_iter)
+    with torch.no_grad():
+        model.eval()
+        pad = EN.vocab.stoi['<pad>']
+        total_loss = 0
+        for b, batch in enumerate(val_iter):
+            src, len_src = batch.src
+            trg, len_trg = batch.trg
+            src = src.data.cuda()
+            trg = trg.data.cuda()
+            output = model(src, trg, teacher_forcing_ratio=0.0)
+            loss = F.nll_loss(output[1:].view(-1, vocab_size),
+                                   trg[1:].contiguous().view(-1),
+                                   ignore_index=pad)
+            total_loss += loss.data[0]
+        return total_loss / len(val_iter)
 
 
 def train(e, model, optimizer, train_iter, vocab_size, grad_clip, DE, EN):
